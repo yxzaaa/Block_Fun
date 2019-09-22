9665dad5db5aa15b2d254e460591fa82b0655337
heywj1994 <15053182523@163.com>
2019-09-22 15:55:46 +0800

    1

--- pages/billorders/billorders.vue
+++ pages/billorders/billorders.vue
@@ -10,49 +10,73 @@
 		<view class="app-container full">
 			<view class="fix-tabs-box">
 				<view class="fix-tabs-item">
-					<text :class="{active:activeTab == 0}" @click="toggleTab(0)">借款挂单</text>
+					<text :class="{active:activeTab == 0}" @click="toggleTab(0)">我的借款</text>
 				</view>
 				<view class="fix-tabs-item">
-					<text :class="{active:activeTab == 1}" @click="toggleTab(1)">投资挂单</text>
+					<text :class="{active:activeTab == 1}" @click="toggleTab(1)">我的投资</text>
 				</view>
 			</view>
 			<swiper class="swiper-box" :current="activeTab" @change="tabChange">
 				<swiper-item>
 					<scroll-view scroll-y='true' style="width:100%;height:100%;">
+						<view class="totalBox">
+							<view class="borrow">
+								<span class="content">借款总额（USDT）</span>
+								<span class="number">5000</span>
+							</view>
+							<view class="pledge">
+								<span class="content">抵押总量（Forest）</span>
+								<span class="number">100000</span>
+							</view>
+						</view>
+						<view class="selected">
+							<span>订单筛选</span>
+							<view class="right-item" style="height:48upx;">
+								<picker @change="pickerChange" :value="currClass" :range="classLib" mode="selector">
+									<view style="padding:0upx 20upx;font-size:24upx;height:100%;background: #2D1F25;line-height: 48upx;color:#fff;display: flex;justify-content: center;align-items: center;">
+										<text style="color:#999;">{{classLib[currClass]}}</text>
+										<image :src="imageLib.sanjiao" style="width:20upx;height:14upx;"></image>
+									</view>
+								</picker>
+							</view>
+							</picker>
+						</view>
 						<view style="padding:40upx;padding-bottom:0px;">
 							<block v-for="(item,index) in borrowList" :key="index">
 								<view class="debitbox">
 									<view class="horizon-list-item">
-										<text>2019/03/15 19:00</text>
-										<text style="color:#DA53A2;">已发布</text>
+										<text style="font-size: 28upx;color:#fff;">进行中</text>
+										<text style="color:#DA53A2;">距离还款日还有10天</text>
+									</view>
+									<view class="order-info">
+										<span>2019/03/15 19:00</span>
+										<span>订单号：
+											<span>asdasdasdasd</span>
+										</span>
 									</view>
 									<view class="debit-info">
-										<view class="debit">
-											<span class="text">USDT价格</span>
-											<span class="number">0.168</span>
+										<view class="borrow">
+											<span class="content">借款总额（USDT）</span>
+											<span class="number">5000</span>
 										</view>
-										<view class="debit">
-											<span class="text">投资总量</span>
-											<span class="number">800</span>
-										</view>
-										<view class="debit">
-											<span class="text">月利率</span>
-											<span class="number">0.1
-												<span>%</span>
-											</span>
-										</view>
-										<view class="debit">
-											<span class="text">月</span>
-											<span class="number">
-												30 <span>天</span>
-											</span>
+										<view class="pledge">
+											<span class="content">抵押总量（Forest）</span>
+											<span class="number">100000</span>
 										</view>
 									</view>
 									<view class="debit-btn">
-										<text>七天后过期</text>
-										<navigator :url="'../borrowpage/borrowpage?id='+index">
-											<view>下架</view>
-										</navigator>
+										<view>
+											<span class="content">综合利率</span>
+											<span class="number">1.5%</span>
+										</view>
+										<view>
+											<span class="content">周期</span>
+											<span class="number">15天</span>
+										</view>
+										<view>
+											<span class="content">还款日</span>
+											<span class="number">2019/4/12</span>
+										</view>
 									</view>
 								</view>
 							</block>
@@ -61,40 +85,64 @@
 				</swiper-item>
 				<swiper-item>
 					<scroll-view scroll-y='true' style="width:100%;height:100%;">
+						<view class="totalBox">
+							<view class="borrow">
+								<span class="content">投资总额（USDT）</span>
+								<span class="number">5000</span>
+							</view>
+							<view class="pledge">
+								<span class="content">我的收益（USTD）</span>
+								<span class="number">100000</span>
+							</view>
+						</view>
+						<view class="selected">
+							<span>订单筛选</span>
+							<view class="right-item" style="height:48upx;">
+								<picker @change="pickerChange" :value="currClass" :range="classLib" mode="selector">
+									<view style="padding:0upx 20upx;font-size:24upx;height:100%;background: #2D1F25;line-height: 48upx;color:#fff;display: flex;justify-content: center;align-items: center;">
+										<text style="color:#999;">{{classLib[currClass]}}</text>
+										<image :src="imageLib.sanjiao" style="width:20upx;height:14upx;"></image>
+									</view>
+								</picker>
+							</view>
+							</picker>
+						</view>
 						<view style="padding:40upx;padding-bottom:0px;">
-							<block v-for="(item,index) in investList" :key="index">
+							<block v-for="(item,index) in borrowList" :key="index">
 								<view class="debitbox">
 									<view class="horizon-list-item">
-										<text>2019/03/15 19:00</text>
-										<text style="color:#DA53A2;">已发布</text>
+										<text style="font-size: 28upx;color:#fff;">进行中</text>
+										<text style="color:#DA53A2;">距离还款日还有10天</text>
+									</view>
+									<view class="order-info">
+										<span>2019/03/15 19:00</span>
+										<span>订单号：
+											<span>asdasdasdasd</span>
+										</span>
 									</view>
 									<view class="debit-info">
-										<view class="debit">
-											<span class="text">USDT价格</span>
-											<span class="number">0.168</span>
+										<view class="borrow">
+											<span class="content">投资金额（USDT）</span>
+											<span class="number">5000</span>
 										</view>
-										<view class="debit">
-											<span class="text">投资总量</span>
-											<span class="number">800</span>
-										</view>
-										<view class="debit">
-											<span class="text">月利率</span>
-											<span class="number">0.1
-												<span>%</span>
-											</span>
-										</view>
-										<view class="debit">
-											<span class="text">月</span>
-											<span class="number">
-												30 <span>天</span>
-											</span>
+										<view class="pledge">
+											<span class="content">到期收益（USDT）</span>
+											<span class="number">100000</span>
 										</view>
 									</view>
 									<view class="debit-btn">
-										<text>七天后过期</text>
-										<navigator :url="'../investpage/investpage?id='+index">
-											<view>下架</view>
-										</navigator>
+										<view>
+											<span class="content">综合利率</span>
+											<span class="number">1.5%</span>
+										</view>
+										<view>
+											<span class="content">周期</span>
+											<span class="number">15天</span>
+										</view>
+										<view>
+											<span class="content">投资结束日</span>
+											<span class="number">2019/4/12</span>
+										</view>
 									</view>
 								</view>
 							</block>
@@ -126,6 +174,12 @@
 					},		
 				},
 				imageLib:{
+					sold:'../../static/icons/icon_sold.png',
+					bought:'../../static/icons/icon_bought.png',
+					alipay:'../../static/icons/icon_alipay.png',
+					wechatpay:'../../static/icons/icon_wechatpay.png',
+					unionpay:'../../static/icons/icon_unionpay.png',
+					sanjiao:'../../static/icons/sanjiao.png',
 					add:'../../static/icons/icon_add.png',
 				},
 				borrowList:[
@@ -133,7 +187,11 @@
 				],
 				investList:[
 					{},{},{}
-				]
+				],
+				currClass:0,
+				classLib:[
+					'全部','进行中','仲裁中','已结束'
+				],
 			}
 		},
 		onPageScroll(val){
@@ -150,6 +208,9 @@
 				uni.navigateTo({
 					url:'../publishborrow/publishborrow'
 				})
+			},
+			pickerChange(e){
+				this.currClass = e.target.value;
 			}
 		}
 	}
@@ -175,61 +236,47 @@
 			}
 		}
 		.debit-info{
-			width:100%;
-			padding:40upx;
 			display: flex;
 			justify-content: space-between;
+			padding:40upx 70upx 50upx;
 			border-bottom:1px solid rgba(255,255,255,0.1);
-			.debit{
+			view{
 				display: flex;
 				flex-direction: column;
-				justify-content: space-between;
-				text-align:center;
-				.text{
-					font-size: 24upx;				
-					color: #FFFFFF;					
+				align-items: center;
+				.content{
+					font-size: 24upx;
+					color:#fff;
 					opacity: 0.5;
-					span{
-						font-size: 24upx;					
-						color: #FFFFFF;
-						margin-left:12upx;
-					}
 				}
 				.number{
-					margin-top:14upx;
-					font-family: 'Montserrat-Bold';
-					font-style: normal;
+					margin-top:10upx;
+					font-size:52upx;
+					color:#fff;
 					font-weight: 600;
-					font-size: 40upx;
-					color: #FFFFFF;
-					span{
-						font-size: 22upx;					
-						color: #FFFFFF;
-						margin-left:8upx;
-					}
 				}
 			}
 		}
 		.debit-btn{
-			width:670upx;
-			padding:30upx 40upx;
+			padding: 30upx 60upx;
 			display:flex;
 			justify-content:space-between;
 			align-items:center;
-			text{
-				color:#DA53A2;
-				font-size: 24upx;
-			}
 			view{
-				width:192upx;
-				background: #DA53A2;
-				border-radius: 200upx;
-				text-align: center;
-				line-height: 64upx;
-				color: #FEFEFE;
-				font-size: 28upx;
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				.content{
+					color:#fff;
+					opacity: 0.5;
+					font-size: 24upx;
+				}
+				.number{
+					color:#fff;
+					font-size: 24upx;
+					margin-top:20upx;
+				}
 			}
-			
 		}
 	}
 	.fiexd-btn{
@@ -248,6 +295,50 @@
 		image{
 			width:64upx;
 			height:64upx;
+		}
+	}
+	.totalBox{
+		width:750upx;
+		display: flex;
+		justify-content: space-between;
+		padding:70upx 120upx;
+		view{
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			.content{
+				font-size: 24upx;
+				color:#fff;
+				opacity: 0.5;
+			}
+			.number{
+				font-size:52upx;
+				color:#fff;
+				font-weight: 600;
+			}
+		}
+	}
+	.selected{
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width:750upx;
+		padding:0 40upx;
+		span{			
+			color: #FFFFFF;
+			font-size: 24upx;
+			opacity: 0.5;
+			margin-top: 10upx;
+		}
+	}
+	.order-info{
+		display: flex;
+		flex-direction: column;
+		padding:16upx 40upx 0upx;
+		span{
+			color:#999;
+			font-size: 24upx;
+			margin-bottom: 12upx;
 		}
 	}
 </style>
